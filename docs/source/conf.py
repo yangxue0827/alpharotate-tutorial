982f6679bdd6d3b33a305a87deab09d36a5e581c
yangxue <yangxue0827@126.com>
2021-09-14 18:42:16 +0800

    update copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AlphaRotate'
+copyright = '2021, XueYang'
+author = 'XueYang'
 
 release = '0.1'
 version = '0.1.0'
